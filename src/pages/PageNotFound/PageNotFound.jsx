2b50757246feb14cc123a0639ba3921e3c0eaa61
zouyos <hamza.benketaf@gmail.com>
2024-03-14 09:46:48 +0100

    css page 404

--- src/pages/PageNotFound/PageNotFound.jsx
+++ src/pages/PageNotFound/PageNotFound.jsx
@@ -2,14 +2,15 @@ import ronflex from "../../assets/img/ronflex.png";
 
 export default function PageNotFound() {
   return (
-    <div className="container p-2 d-flex flex-column align-items-center">
-      <img
-        src={ronflex}
-        alt="Ronflex"
-        className="col-6"
-        style={{ marginTop: "50px" }}
-      />
-      <h1 className="text-center">Erreur 404 - Page Introuvable</h1>
+    <div className="container p-2">
+      <div className="d-flex justify-content-center">
+        <img
+          src={ronflex}
+          alt="Ronflex"
+          style={{ marginTop: "50px", width: "300px" }}
+        />
+      </div>
+      <h1 className="text-center text-wrap">Erreur 404 - Page Introuvable</h1>
     </div>
   );
 }
